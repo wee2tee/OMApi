b2621bdd97ca9ffc505d93415e8a0eb2220e1ae7
weerawat.36@gmail.com <Wee2Tee01121976>
2016-09-12 17:43:22 +0700

    adding Models/InternalUsers

--- OMApi/Controllers/UsersController.cs
+++ OMApi/Controllers/UsersController.cs
@@ -4,10 +4,23 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using OMApi.Models;
 
 namespace OMApi.Controllers
 {
     public class UsersController : ApiController
     {
+        OMApiEntities db = new OMApiEntities();
+
+        public HttpResponseMessage PostUsers([FromBody] InternalUsers users )
+        {
+            db.InternalUsers.Add(users);
+            db.SaveChanges();
+            var response = Request.CreateResponse<InternalUsers>(HttpStatusCode.Created, users);
+
+            string uri = Url.Link("DefaultApi", new { id = users.Id });
+            response.Headers.Location = new Uri(uri);
+            return response;
+        }
     }
 }
